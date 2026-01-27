687aaa8c6a577608f307b83e4018751e255bbce6
yuli <yuli33@proton.me>
2026-01-27 14:26:44 +0300

    Revert "chore(deps): update plugin com.android.library to v9 (#1743)"
    This reverts commit 6fb5d0128eaea12d99dd775e897a9c8fcb8d4cfe.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 plugins {
     id("com.android.application")
+    id("org.jetbrains.kotlin.android")
     id("org.jetbrains.kotlin.plugin.compose")
     id("com.google.devtools.ksp")
     kotlin("plugin.serialization") version "2.3.0"
