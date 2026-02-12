abd55d4c3494d08e8a22a6ab53190c56c1c1cd1a
yuli <yuli33@proton.me>
2026-02-12 20:46:23 +0300

    implemented shift and numeric layers

--- app/src/main/java/com/dessalines/thumbkey/keyboards/ENChord.kt
+++ app/src/main/java/com/dessalines/thumbkey/keyboards/ENChord.kt
@@ -3,101 +3,160 @@
 package com.dessalines.thumbkey.keyboards
 
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.outlined.KeyboardReturn
 import androidx.compose.material.icons.outlined.ArrowDropDown
 import androidx.compose.material.icons.outlined.ArrowDropUp
 import androidx.compose.material.icons.outlined.Copyright
 import androidx.compose.material.icons.outlined.KeyboardCapslock
+import androidx.compose.material.icons.outlined.Numbers
 import com.dessalines.thumbkey.utils.*
 import com.dessalines.thumbkey.utils.ColorVariant.*
 import com.dessalines.thumbkey.utils.FontSizeVariant.*
 import com.dessalines.thumbkey.utils.KeyAction.*
 import com.dessalines.thumbkey.utils.SwipeNWay.*
 
-val KB_EN_CHORD_MAIN =
+val KB_EN_TEAPOT_MAIN =
     KeyboardC(
         listOf(
             listOf(
                 KeyItemC(
-                    center = KeyC(7, "r", size=LARGE)
+                    center = KeyC(1 shl 7, "r", size=LARGE)
                 ),
                 KeyItemC(
-                    center = KeyC(6, "t", size=LARGE)
+                    center = KeyC(1 shl 6, "t", size=LARGE)
                 ),
                 KeyItemC(
-                    center = KeyC(5, "e", size=LARGE)
+                    center = KeyC(1 shl 5, "e", size=LARGE)
                 ),
                 KeyItemC(
-                    center = KeyC(4, "a", size=LARGE)
+                    center = KeyC(1 shl 4, "a", size=LARGE)
                 ),
             ),
             listOf(
                 KeyItemC(
-                    center = KeyC(3, "n", size=LARGE)
+                    center = KeyC(1 shl 3, "n", size=LARGE)
                 ),
                 KeyItemC(
-                    center = KeyC(2, "o", size=LARGE)
+                    center = KeyC(1 shl 2, "o", size=LARGE)
                 ),
                 KeyItemC(
-                    center = KeyC(1, "s", size=LARGE)
+                    center = KeyC(1 shl 1, "s", size=LARGE)
                 ),
                 KeyItemC(
-                    center = KeyC(0, "i", size=LARGE)
+                    center = KeyC(1 shl 0, "i", size=LARGE)
                 ),
             ),
             listOf(
-                SPACEBAR_DOUBLE_KEY_ITEM,
+                KeyItemC(
+                    center = KeyC(1 shl 9),
+                    widthMultiplier = 2,
+                    backgroundColor = SURFACE_VARIANT
+                ),
                 BACKSPACE_KEY_ITEM,
-                RETURN_KEY_ITEM,
+                KeyItemC(
+                    swipeType = FOUR_WAY_CROSS,
+                    backgroundColor = SURFACE_VARIANT,
+                    // TODO: implement these as modified existing keyCs instead of making them from scratch
+                    center = KeyC(
+                        display = KeyDisplay.IconDisplay(Icons.Outlined.ArrowDropUp),
+                        action = ToggleShiftMode(true),
+                        color = SECONDARY,
+                        size = LARGE,
+                    ),
+                    top = KeyC(
+                        display = KeyDisplay.IconDisplay(Icons.AutoMirrored.Outlined.KeyboardReturn),
+                        action = IMECompleteAction,
+                        size = SMALL,
+                        color = MUTED,
+                    ),
+                    left = KeyC(
+                        display = KeyDisplay.IconDisplay(Icons.Outlined.Numbers),
+                        action = ToggleNumericMode(true),
+                        size = SMALL,
+                        color = MUTED,
+                    )
+                )
             )
         )
     )
 
-val KB_EN_CHORD_SHIFTED =
+val KB_EN_TEAPOT_SHIFTED =
     KeyboardC(
         listOf(
             listOf(
                 KeyItemC(
-                    center = KeyC(0, "i", size=LARGE)
+                    center = KeyC((1 shl 8) or (1 shl 7), "R", size=LARGE)
                 ),
                 KeyItemC(
-                    center = KeyC(1, "n", size=LARGE)
+                    center = KeyC((1 shl 8) or (1 shl 6), "T", size=LARGE)
                 ),
                 KeyItemC(
-                    center = KeyC(2, "s", size=LARGE)
+                    center = KeyC((1 shl 8) or (1 shl 5), "E", size=LARGE)
                 ),
                 KeyItemC(
-                    center = KeyC(3, "r", size=LARGE)
+                    center = KeyC((1 shl 8) or (1 shl 4), "A", size=LARGE)
                 ),
             ),
             listOf(
                 KeyItemC(
-                    center = KeyC(4, "e", size=LARGE)
+                    center = KeyC((1 shl 8) or (1 shl 3), "N", size=LARGE)
                 ),
                 KeyItemC(
-                    center = KeyC(5, "t", size=LARGE)
+                    center = KeyC((1 shl 8) or (1 shl 2), "O", size=LARGE)
                 ),
                 KeyItemC(
-                    center = KeyC(6, "o", size=LARGE)
+                    center = KeyC((1 shl 8) or (1 shl 1), "S", size=LARGE)
                 ),
                 KeyItemC(
-                    center = KeyC(7, "a", size=LARGE)
+                    center = KeyC((1 shl 8) or (1 shl 0), "I", size=LARGE)
                 ),
             ),
             listOf(
-                SPACEBAR_DOUBLE_KEY_ITEM,
+                KeyItemC(
+                    center = KeyC(1 shl 9),
+                    widthMultiplier = 2,
+                    backgroundColor = SURFACE_VARIANT
+                ),
                 BACKSPACE_KEY_ITEM,
-                RETURN_KEY_ITEM
+                KeyItemC(
+                    swipeType = FOUR_WAY_CROSS,
+                    backgroundColor = SURFACE_VARIANT,
+                    center = KeyC(
+                        display = KeyDisplay.IconDisplay(Icons.Outlined.KeyboardCapslock),
+                        capsModeDisplay = KeyDisplay.IconDisplay(Icons.Outlined.Copyright),
+                        action = ToggleCapsLock,
+                        color = SECONDARY,
+                        size = LARGE,
+                    ),
+                    top = KeyC(
+                        display = KeyDisplay.IconDisplay(Icons.AutoMirrored.Outlined.KeyboardReturn),
+                        action = IMECompleteAction,
+                        size = SMALL,
+                        color = SECONDARY,
+                    ),
+                    left = KeyC(
+                        display = KeyDisplay.IconDisplay(Icons.Outlined.Numbers),
+                        action = ToggleNumericMode(true),
+                        size = SMALL,
+                        color = SECONDARY,
+                    ),
+                    bottom = KeyC(
+                        display = KeyDisplay.IconDisplay(Icons.Outlined.ArrowDropDown),
+                        action = ToggleShiftMode(false),
+                        color = MUTED,
+                    )
+                )
             )
         )
     )
 
-val KB_EN_CHORD: KeyboardDefinition =
+val KB_EN_TEAPOT: KeyboardDefinition =
     KeyboardDefinition(
-        title = "english chord",
+        title = "english teapot",
         modes =
             KeyboardDefinitionModes(
-                main = KB_EN_CHORD_MAIN,
-                shifted = KB_EN_CHORD_SHIFTED,
+                main = KB_EN_TEAPOT_MAIN,
+                shifted = KB_EN_TEAPOT_SHIFTED,
                 numeric = NUMERIC_KEYBOARD,
             ),
         settings =
--- app/src/main/java/com/dessalines/thumbkey/utils/ChordManager.kt
+++ app/src/main/java/com/dessalines/thumbkey/utils/ChordManager.kt
@@ -1,49 +1,13 @@
 package com.dessalines.thumbkey.utils
 
+import android.util.Log
+
 class ChordManager {
     private var state = 0
     private var curChord = 0
 
-    private val dict = mapOf(0 to "",
-//        0b1000_0000 to "i",
-//        0b0100_0000 to "n",
-//        0b0010_0000 to "s",
-//        0b0001_0000 to "r",
-//
-//        0b0000_1000 to "e",
-//        0b0000_0100 to "t",
-//        0b0000_0010 to "o",
-//        0b0000_0001 to "a",
-//
-//        0b1100_0000 to "y",
-//        0b0011_0000 to "b",
-//        0b0000_1100 to "h",
-//        0b0000_0011 to "l",
-//
-//        0b1001_0000 to "j", // gz swap + jz swap
-//        0b0000_0110 to "u",
-//        0b0000_1001 to "q", // dq swap
-//        0b0110_0000 to "p",
-//
-//        0b0000_1010 to "c",
-//        0b0000_0101 to "d", // dq swap
-//        0b1010_0000 to "f",
-//        0b0101_0000 to "g", // gz swap
-//
-//        0b0010_1000 to "v",
-//        0b0001_0100 to "k", // kx swap
-//        0b1000_0010 to "x", // kx swap
-//        0b0100_0001 to "z", // jz swap
-//
-//        0b0100_0010 to "_",
-//        0b1000_0001 to ";", // w; swap
-//        0b0001_1000 to "/", // m/ swap
-//        0b0010_0100 to "m", // m/ swap
-//
-//        0b0010_0001 to "'",
-//        0b0001_0010 to "w", // w; swap
-//        0b1000_0100 to "?",
-//        0b0100_1000 to ",",
+    private val dictAlpha = mapOf(
+        0b10_0000_0000 to " ",
 
         0b1000_0000 to "r",
         0b0100_0000 to "t",
@@ -56,24 +20,20 @@ class ChordManager {
         0b0000_0001 to "i",
 
         0b1100_0000 to "v",
+        0b0110_0000 to "m",
         0b0011_0000 to "h",
+
         0b0000_1100 to "y",
+        0b0000_0110 to "u",
         0b0000_0011 to "p",
 
-        0b1001_0000 to ",",
-        0b0000_0110 to "u",
-        0b0000_1001 to ".",
-        0b0110_0000 to "m",
-
-        0b0000_1010 to "g",
-        0b0000_0101 to "f",
         0b1010_0000 to "b",
         0b0101_0000 to "j",
+        0b0000_1010 to "g",
+        0b0000_0101 to "f",
 
         0b0010_1000 to "w",
         0b0001_0100 to "k",
-        0b1000_0010 to "'",
-        0b0100_0001 to "?",
 
         0b0100_1000 to "d",
         0b0010_0100 to "l",
@@ -83,25 +43,93 @@ class ChordManager {
         0b0100_0100 to "q",
         0b0010_0010 to "x",
         0b0001_0001 to "z",
+
+        0b1000_0100 to "z",
+        0b0100_0010 to "q",
+        0b0010_0001 to "x",
+
+        0b10_1000_0000 to "@",
+        0b10_0100_0000 to ".",
+        0b10_0010_0000 to "?",
+        0b10_0001_0000 to "\"",
+
+        0b10_0000_1000 to ":",
+        0b10_0000_0100 to ",",
+        0b10_0000_0010 to "'",
+        0b10_0000_0001 to "!",
     )
 
-    // TODO: consider merging down and up into toggle
-    //  that would b less repeating code but seems possibly breakable mayb ??
-    fun keyDown(key: Int) {
-        assert((state and (1 shl key)) == 0)
+    private val dictShifted = mapOf(
+        0b11_0000_0000 to " ",
 
-        state = state or (1 shl key)
-        curChord = curChord or (1 shl key)
+        0b1_1000_0000 to "R",
+        0b1_0100_0000 to "T",
+        0b1_0010_0000 to "E",
+        0b1_0001_0000 to "A",
+
+        0b1_0000_1000 to "N",
+        0b1_0000_0100 to "O",
+        0b1_0000_0010 to "S",
+        0b1_0000_0001 to "I",
+
+        0b1_1100_0000 to "V",
+        0b1_0110_0000 to "M",
+        0b1_0011_0000 to "H",
+
+        0b1_0000_1100 to "Y",
+        0b1_0000_0110 to "U",
+        0b1_0000_0011 to "P",
+
+        0b1_1010_0000 to "B",
+        0b1_0101_0000 to "J",
+        0b1_0000_1010 to "G",
+        0b1_0000_0101 to "F",
+
+        0b1_0010_1000 to "W",
+        0b1_0001_0100 to "K",
+
+        0b1_0100_1000 to "D",
+        0b1_0010_0100 to "L",
+        0b1_0001_0010 to "C",
+
+        0b1_1000_1000 to ":3",
+        0b1_0100_0100 to "Q",
+        0b1_0010_0010 to "X",
+        0b1_0001_0001 to "Z",
+
+        0b1_1000_0100 to "Z",
+        0b1_0100_0010 to "Q",
+        0b1_0010_0001 to "X",
+
+        0b11_1000_0000 to "@",
+        0b11_0100_0000 to ".",
+        0b11_0010_0000 to "?",
+        0b11_0001_0000 to "\"",
+
+        0b11_0000_1000 to ":",
+        0b11_0000_0100 to ",",
+        0b11_0000_0010 to "'",
+        0b11_0000_0001 to "!",
+    )
+
+    private val dict = dictAlpha + dictShifted
+    
+    fun keyDown(keyMask: Int) {
+        state = state or keyMask
+        curChord = curChord or keyMask
+
+        Log.i("state", state.toString())
     }
 
-    fun keyUp(key: Int): String? {
-        assert((state and (1 shl key)) != 0)
+    fun keyUp(keyMask: Int): String? {
+        state = state and (-1 xor keyMask)
 
-        state = state xor (1 shl key)
-
+        Log.i("state", state.toString())
         if (state != 0) { // some keys are still held
             return null
         }
+
+        Log.i("curChord", curChord.toString())
 
         // chord done
         val res = dict[curChord]
